1a264897b9c7a47b351301b552cde9988813f334
Mateuzo <mateushcandido5@gmail.com>
2025-01-30 16:20:29 -0300

    adjusting on datas for dashboard

--- scripts/dml/dashboard_selects.sql
+++ scripts/dml/dashboard_selects.sql
@@ -1,36 +1,29 @@
---Number of orders by day
+--Total order by date and payment type
 SELECT
-	DATE(d_tim.time_date) AS order_day,
-	COUNT(d_oft.*) AS total_orders
+	DATE(d_tim.time_date) AS "order day",
+	d_tim.time_month AS "month",
+	d_tim.time_day AS "day",
+	d_tim.time_year AS "year",
+	REPLACE(d_orp.payment_type, '_', ' ') AS "payment type",
+	COUNT(d_oft.*) AS "total orders"
 FROM
 	dimensional.orders_fact d_oft
 INNER JOIN
 	dimensional.dim_time d_tim
 ON
 	d_oft.time_key = d_tim.time_key
+INNER JOIN	
+	dimensional.order_payments d_orp
+ON
+	d_oft.order_payment_key = d_orp.order_payment_key
 GROUP BY
-	DATE(time_date)
+	"payment type",
+	"order day",
+	"month",
+	"day",
+	"year"
 ORDER BY
-	order_day
-
-
---Number of reviews by product
-SELECT 
-    d_pro.product_id, 
-    d_pro.product_category_name,
-    COUNT(*) AS total_reviews
-FROM 
-    dimensional.fact_order_reviews d_for
-INNER JOIN
-    dimensional.products d_pro
-ON	
-    d_for.product_key = d_pro.product_key
-GROUP BY
-    d_pro.product_id, 
-    d_pro.product_category_name
-ORDER BY
-    total_reviews DESC
-
+	"order day";
 
 --Number of orders by location
 SELECT 
@@ -56,20 +49,33 @@ ORDER BY
 	geolocation_state
 
 
---Number of orders by payment type
+--Number of reviews by product
 SELECT 
-	d_orp.payment_type,
-	COUNT(d_oft.*) AS total_orders
+	d_rev.review_id AS "review id",
+	d_pro.product_id AS "product id",
+	REPLACE(d_pro.product_category_name, '_', ' ') AS "product category name",
+	d_cus.customer_state AS "customer state",
+	d_cus.customer_city AS "customer city",
+	COUNT(*) AS "total reviews"
+	
 FROM
-	dimensional.orders_fact d_oft
+	dimensional.fact_order_reviews d_rev
 INNER JOIN
-	dimensional.order_payments d_orp
+	dimensional.customers d_cus
 ON
-	d_oft.order_payment_key = d_orp.order_payment_key
+	d_rev.customer_key = d_cus.customer_key
+INNER JOIN
+	dimensional.products d_pro
+ON
+	d_rev.product_key = d_pro.product_key
 GROUP BY
-	d_orp.payment_type
+	review_id,
+	product_id,
+	"product category name",
+	customer_state,
+	customer_city
 ORDER BY
-	total_orders DESC;
+	"total reviews" DESC ;
 
 
 --Number of reviews by customer
